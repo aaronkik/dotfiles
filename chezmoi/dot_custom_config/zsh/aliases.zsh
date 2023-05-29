ae1b98927861c8dff83c3996c447edcaae1c1b8c
Aaron Kikabhai <37105197+aaronkik@users.noreply.github.com>
2023-05-29 22:14:12 +0100

    Add zsh aliases

---
+++ chezmoi/dot_custom_config/zsh/aliases.zsh
@@ -0,0 +1,10 @@
+alias ls=exa
+alias lsa='exa -lah'
+
+alias gco='git checkout'
+alias gs='git status'
+alias ga='git add'
+alias gap='git add -p'
+alias gc='git commit'
+alias grpo='git remote prune origin'
+alias ff='grpo && git pull --ff-only'
