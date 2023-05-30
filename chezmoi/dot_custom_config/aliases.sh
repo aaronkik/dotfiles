64faf3cf7fdd219b471a9db3e0772a1915749d56
Aaron Kikabhai <37105197+aaronkik@users.noreply.github.com>
2023-05-30 21:16:52 +0100

    Add uuid alias

--- chezmoi/dot_custom_config/aliases.sh
+++ chezmoi/dot_custom_config/aliases.sh
@@ -3,6 +3,7 @@ alias lsa='exa -lah'
 alias h=history
 alias jj='pbpaste | jsonpp | pbcopy'
 alias rm=trash
+alias uuid='uuidgen | tr "[:upper:]" "[:lower:]" | pbcopy'
 
 alias gco='git checkout'
 alias gs='git status'
