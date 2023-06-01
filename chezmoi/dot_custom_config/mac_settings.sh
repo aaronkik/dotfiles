4fbbc2ec03a9b609a6a130396ab94e6128cfd8c7
Aaron Kikabhai <119860087+aaronkikabhai-cinch@users.noreply.github.com>
2023-06-01 08:22:22 +0100

    comment out kill

--- chezmoi/dot_custom_config/mac_settings.sh
+++ chezmoi/dot_custom_config/mac_settings.sh
@@ -20,8 +20,8 @@ osascript -e "$set_terminal_font_and_size"
 # Kill affected applications                                                  #
 ###############################################################################
 
-for app in "Terminal"; 
-    do killall "${app}" &> /dev/null
-done
+# for app in "Terminal"; 
+#     do killall "${app}" &> /dev/null
+# done
 
-echo "\033[33mAffected applications killed. Some of these changes may require a logout/restart to take effect.\033[m"
+# echo "\033[33mAffected applications killed. Some of these changes may require a logout/restart to take effect.\033[m"
