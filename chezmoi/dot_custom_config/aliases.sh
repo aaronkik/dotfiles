009796eac7ff70b3624b36ee1808fe18a347a4e6
Aaron Kikabhai <37105197+aaronkik@users.noreply.github.com>
2024-01-27 21:48:41 +0000

    aliases 😎

--- chezmoi/dot_custom_config/aliases.sh
+++ chezmoi/dot_custom_config/aliases.sh
@@ -1,12 +1,19 @@
 alias c=clear
+alias epoch='date +%s'
+alias iso='epoch | jq todate | pbcopy'
 alias l=exa
 alias ls='exa -lah'
 alias h='history -E'
 alias jj='pbpaste | jsonpp -s | pbcopy'
 alias rm=trash
 alias s="source ~/.zshrc"
+alias to_epoch='pbpaste | jq fromdate | pbcopy'
+alias to_iso='pbpaste | jq todate | pbcopy'
+alias to_json='pbpaste | jq fromjson | pbcopy'
+alias to_string='pbpaste | jq tostring | pbcopy'
 alias uuid='uuidgen | tr -d "\n" | tr "[:upper:]" "[:lower:]" | pbcopy'
 
+# Git aliases
 alias gco='git checkout'
 alias gs='git status'
 alias ga='git add'
