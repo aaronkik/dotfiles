8d8e8d2ee2313f567034fe3d1cdbb79772cd7e9d
Aaron Kikabhai <37105197+aaronkik@users.noreply.github.com>
2024-01-27 18:50:19 +0000

    replace fig with codewhisperer

---
+++ chezmoi/dot_custom_config/codewhisperer.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cw settings autocomplete.fontFamily "FiraCode Nerd Font Retina"
+cw settings autocomplete.fontSize 14
+cw settings autocomplete.height 200
+cw settings autocomplete.width 450
+cw settings app.hideMenubarIcon true
+cw settings codeWhisperer.shareCodeWhispererContentWithAWS false
+cw settings integrations.hyper.disabled true
+cw settings integrations.iterm.disabled true
+cw settings integrations.terminal.disabled false
+cw settings integrations.vscode.disabled false
+cw settings telemetry.enabled false
+cw theme dracula
--- chezmoi/dot_custom_config/fig.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-fig settings autocomplete.fontFamily "FiraCode Nerd Font Retina"
-fig settings autocomplete.fontSize 14
-fig settings autocomplete.height 200
-fig settings autocomplete.width 450
-fig telemetry disable
-fig theme dracula
