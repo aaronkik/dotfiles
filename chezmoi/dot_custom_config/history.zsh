d0301c1845026b5dee805891c1d3b9ea171799c7
Aaron Kikabhai <37105197+aaronkik@users.noreply.github.com>
2024-05-26 01:22:54 +0100

    chore: try new history bindings

--- chezmoi/dot_custom_config/history.zsh
+++ chezmoi/dot_custom_config/history.zsh
@@ -39,4 +39,7 @@ setopt hist_ignore_all_dups   # delete all duplicates from history
 setopt hist_ignore_space      # ignore commands that start with space
 setopt hist_verify            # show command with history expansion to user before running it
 setopt inc_append_history     # add commands to HISTFILE in order of execution
-# setopt share_history        # share command history data
+setopt share_history        # share command history data
+
+bindkey "^[[A" history-search-backward
+bindkey "^[[B" history-search-forward
