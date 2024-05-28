0268371718e810382d83d5842a04325b1a866b3a
Aaron Kikabhai <37105197+aaronkik@users.noreply.github.com>
2024-05-28 09:36:42 +0100

    chore: default mac setting

--- chezmoi/dot_custom_config/mac_settings.sh
+++ chezmoi/dot_custom_config/mac_settings.sh
@@ -6,22 +6,8 @@
 # settings we’re about to change
 osascript -e 'tell application "System Preferences" to quit'
 
-set_terminal_font_and_size='
-tell application "Terminal"
-    set current settings of front window to settings set "Basic"
-    set font name of front window to "FiraCode Nerd Font Retina"
-    set font size of front window to 18
-end tell
-'
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -float 0
 
-osascript -e "$set_terminal_font_and_size"
-
-###############################################################################
-# Kill affected applications                                                  #
-###############################################################################
-
-# for app in "Terminal"; 
-#     do killall "${app}" &> /dev/null
-# done
-
-# echo "\033[33mAffected applications killed. Some of these changes may require a logout/restart to take effect.\033[m"
+defaults write com.apple.finder ShowPathbar -bool true
