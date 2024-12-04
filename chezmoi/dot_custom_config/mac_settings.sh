aa8672cd6afd9f273292be6759c0b0fd9e889082
Aaron Kikabhai <37105197+aaronkik@users.noreply.github.com>
2024-12-04 09:34:16 +0000

    chore: update mac settings

--- chezmoi/dot_custom_config/mac_settings.sh
+++ chezmoi/dot_custom_config/mac_settings.sh
@@ -6,10 +6,22 @@
 # settings we’re about to change
 osascript -e 'tell application "System Preferences" to quit'
 
+defaults write com.apple.Accessibility EnhancedBackgroundContrastEnabled -float 1
+
+defaults write com.apple.AdLib allowApplePersonalizedAdvertising -float 0
+defaults write com.apple.AdLib allowIdentifierForAdvertising -float 0
+
+defaults write com.apple.controlcenter NSStatusItem-Visible-Battery -float 1
+
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock autohide-delay -float 0
 defaults write com.apple.dock autohide-time-modifier -float 0
 
 defaults write com.apple.finder ShowPathbar -bool true
+defaults write com.apple.finder FXRemoveOldTrashItems -float 1
+defaults write com.apple.finder ShowRecentTags -float 0
+
+defaults write com.apple.menuextra.clock ShowSeconds -float 1
 
 defaults write com.apple.WindowManager EnableTiledWindowMargins -float 0
+
