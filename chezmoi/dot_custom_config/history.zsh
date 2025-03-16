5f7a94f6950b018f7c9935808abef3494ecd0c6a
Aaron Kikabhai <37105197+aaronkik@users.noreply.github.com>
2025-03-16 21:49:15 +0000

    chore: remove bindings from history

--- chezmoi/dot_custom_config/history.zsh
+++ chezmoi/dot_custom_config/history.zsh
@@ -40,6 +40,3 @@ setopt hist_ignore_space      # ignore commands that start with space
 setopt hist_verify            # show command with history expansion to user before running it
 setopt inc_append_history     # add commands to HISTFILE in order of execution
 setopt share_history          # share command history data
-
-bindkey "^[[A" history-search-backward
-bindkey "^[[B" history-search-forward
