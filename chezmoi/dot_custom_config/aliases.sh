04e03e9ba5cef067d0778e030b02f01c395350cb
Aaron Kikabhai <37105197+aaronkik@users.noreply.github.com>
2026-03-10 20:54:27 +0000

    feat(aliases): add brew update and upgrade alias

--- chezmoi/dot_custom_config/aliases.sh
+++ chezmoi/dot_custom_config/aliases.sh
@@ -14,6 +14,7 @@ alias to_string='pbpaste | jq --raw-output tostring | pbcopy'
 alias ide='goland nosplash'
 alias uuid='uuidgen | tr -d "\n" | tr "[:upper:]" "[:lower:]" | pbcopy'
 alias cat=bat
+alias bup='brew update && brew upgrade'
 
 # Git aliases
 alias sw='git switch'
